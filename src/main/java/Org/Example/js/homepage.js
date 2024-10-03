b36c989ce1266fce64f9d262bb04fa418cd1fc4e
Magnus Munk Braunstein <147395199+MagnusMunkBraunstein@users.noreply.github.com>
2024-10-03 10:51:52 +0200

    Init and frontpage for 3 types of employee

---
+++ src/main/java/Org/Example/js/homepage.js
@@ -0,0 +1,13 @@
+
+// Redirect to pages
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('season-button').addEventListener('click', function() {
+        window.location.href = 'seasonemployee.html';
+    });
+    document.getElementById('service-button').addEventListener('click', function() {
+        window.location.href = 'serviceemployee.html';
+    });
+    document.getElementById('sales-button').addEventListener('click', function() {
+        window.location.href = 'salesemployee.html';
+    });
+});
