8eb416acf03e38accaf79b390774615226ba4206
Aleksander Hvid Ellegård <142972647+AlekOmOm@users.noreply.github.com>
2024-10-11 14:32:45 +0200

    endpoints for startpage

--- src/main/java/Org/Example/js/homepage.js
+++ src/main/java/Org/Example/js/homepage.js
@@ -2,7 +2,7 @@
 // Redirect to pages
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('season-button').addEventListener('click', function() {
-        window.location.href = 'seasonemployee.html';
+        window.location.href = 'booking.html';
     });
     document.getElementById('service-button').addEventListener('click', function() {
         window.location.href = 'serviceemployee.html';
